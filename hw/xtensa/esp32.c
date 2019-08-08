235ac8c771cda5dd0108e9ae8aa4b9f8ef2c5038
Ivan Grokhotkov <ivan@espressif.com>
2019-08-08 17:24:01 +0200

    hw/xtensa/esp32: add 2nd CPU, dport, rtc_cntl

--- hw/xtensa/esp32.c
+++ hw/xtensa/esp32.c
@@ -17,8 +17,11 @@
 #include "hw/loader.h"
 #include "hw/sysbus.h"
 #include "target/xtensa/cpu.h"
+#include "hw/misc/esp32_reg.h"
 #include "hw/char/esp32_uart.h"
 #include "hw/misc/esp32_gpio.h"
+#include "hw/misc/esp32_dport.h"
+#include "hw/misc/esp32_rtc_cntl.h"
 #include "hw/xtensa/xtensa_memory.h"
 #include "hw/misc/unimp.h"
 #include "elf.h"
@@ -26,68 +29,10 @@
 #define TYPE_ESP32_SOC "xtensa.esp32"
 #define ESP32_SOC(obj) OBJECT_CHECK(Esp32SocState, (obj), TYPE_ESP32_SOC)
 
-#define ESP32_SOC_REG_CNT    0x100
-
 #define TYPE_ESP32_CPU XTENSA_CPU_TYPE_NAME("esp32")
 
 typedef struct XtensaCPU XtensaCPU;
 
-
-#define DR_REG_DPORT_BASE                       0x3ff00000
-#define DR_REG_AES_BASE                         0x3ff01000
-#define DR_REG_RSA_BASE                         0x3ff02000
-#define DR_REG_SHA_BASE                         0x3ff03000
-#define DR_REG_FLASH_MMU_TABLE_PRO              0x3ff10000
-#define DR_REG_FLASH_MMU_TABLE_APP              0x3ff12000
-#define DR_REG_DPORT_END                        0x3ff13FFC
-#define DR_REG_UART_BASE                        0x3ff40000
-#define DR_REG_SPI1_BASE                        0x3ff42000
-#define DR_REG_SPI0_BASE                        0x3ff43000
-#define DR_REG_GPIO_BASE                        0x3ff44000
-#define DR_REG_GPIO_SD_BASE                     0x3ff44f00
-#define DR_REG_FE2_BASE                         0x3ff45000
-#define DR_REG_FE_BASE                          0x3ff46000
-#define DR_REG_FRC_TIMER_BASE                   0x3ff47000
-#define DR_REG_RTCCNTL_BASE                     0x3ff48000
-#define DR_REG_RTCIO_BASE                       0x3ff48400
-#define DR_REG_SENS_BASE                        0x3ff48800
-#define DR_REG_RTC_I2C_BASE                     0x3ff48C00
-#define DR_REG_IO_MUX_BASE                      0x3ff49000
-#define DR_REG_HINF_BASE                        0x3ff4B000
-#define DR_REG_UHCI1_BASE                       0x3ff4C000
-#define DR_REG_I2S_BASE                         0x3ff4F000
-#define DR_REG_UART1_BASE                       0x3ff50000
-#define DR_REG_BT_BASE                          0x3ff51000
-#define DR_REG_I2C_EXT_BASE                     0x3ff53000
-#define DR_REG_UHCI0_BASE                       0x3ff54000
-#define DR_REG_SLCHOST_BASE                     0x3ff55000
-#define DR_REG_RMT_BASE                         0x3ff56000
-#define DR_REG_PCNT_BASE                        0x3ff57000
-#define DR_REG_SLC_BASE                         0x3ff58000
-#define DR_REG_LEDC_BASE                        0x3ff59000
-#define DR_REG_EFUSE_BASE                       0x3ff5A000
-#define DR_REG_SPI_ENCRYPT_BASE                 0x3ff5B000
-#define DR_REG_NRX_BASE                         0x3ff5CC00
-#define DR_REG_BB_BASE                          0x3ff5D000
-#define DR_REG_PWM_BASE                         0x3ff5E000
-#define DR_REG_TIMERGROUP0_BASE                 0x3ff5F000
-#define DR_REG_TIMERGROUP1_BASE                 0x3ff60000
-#define DR_REG_RTCMEM0_BASE                     0x3ff61000
-#define DR_REG_RTCMEM1_BASE                     0x3ff62000
-#define DR_REG_RTCMEM2_BASE                     0x3ff63000
-#define DR_REG_SPI2_BASE                        0x3ff64000
-#define DR_REG_SPI3_BASE                        0x3ff65000
-#define DR_REG_SYSCON_BASE                      0x3ff66000
-#define DR_REG_APB_CTRL_BASE                    0x3ff66000    /* Old name for SYSCON, to be removed */
-#define DR_REG_I2C1_EXT_BASE                    0x3ff67000
-#define DR_REG_SDMMC_BASE                       0x3ff68000
-#define DR_REG_EMAC_BASE                        0x3ff69000
-#define DR_REG_CAN_BASE                         0x3ff6B000
-#define DR_REG_PWM1_BASE                        0x3ff6C000
-#define DR_REG_I2S1_BASE                        0x3ff6D000
-#define DR_REG_UART2_BASE                       0x3ff6E000
-#define DR_REG_PWM2_BASE                        0x3ff6F000
-#define DR_REG_PWM3_BASE                        0x3ff70000
 
 
 enum {
@@ -95,6 +40,8 @@ enum {
     ESP32_MEMREGION_DROM,
     ESP32_MEMREGION_DRAM,
     ESP32_MEMREGION_IRAM,
+    ESP32_MEMREGION_ICACHE0,
+    ESP32_MEMREGION_ICACHE1,
     ESP32_MEMREGION_UART
 };
 
@@ -106,6 +53,8 @@ static const struct MemmapEntry {
     [ESP32_MEMREGION_IROM] = { 0x40000000, 0x70000 },
     [ESP32_MEMREGION_DRAM] = { 0x3ffae000, 0x52000 },
     [ESP32_MEMREGION_IRAM] = { 0x40080000, 0x20000 },
+    [ESP32_MEMREGION_ICACHE0] = { 0x40070000, 0x8000 },
+    [ESP32_MEMREGION_ICACHE1] = { 0x40078000, 0x8000 },
     [ESP32_MEMREGION_UART] = { 0x3ff40000, 0x1000 },
 };
 
@@ -114,20 +63,29 @@ typedef struct Esp32SocState {
     SysBusDevice parent_obj;
 
     /*< public >*/
-    XtensaCPU cpu;
+    XtensaCPU cpu[ESP32_CPU_COUNT];
+    Esp32DportState dport;
     ESP32UARTState uart;
     Esp32GpioState gpio;
+    Esp32RtcCntlState rtc_cntl;
 } Esp32SocState;
 
 
 static void esp32_soc_reset(DeviceState *dev)
 {
     Esp32SocState *s = ESP32_SOC(dev);
+    MachineState *ms = MACHINE(qdev_get_machine());
+
+    for (int i = 0; i < ms->smp.cpus; ++i) {
+        cpu_reset(CPU(&s->cpu));
+        xtensa_runstall(&s->cpu[i].env, i != 0);
+    }
 }
 
 static void esp32_soc_realize(DeviceState *dev, Error **errp)
 {
     Esp32SocState *s = ESP32_SOC(dev);
+    MachineState *ms = MACHINE(qdev_get_machine());
 
     const struct MemmapEntry *memmap = esp32_memmap;
     MemoryRegion *sys_mem = get_system_memory();
@@ -136,6 +94,8 @@ static void esp32_soc_realize(DeviceState *dev, Error **errp)
     MemoryRegion *iram = g_new(MemoryRegion, 1);
     MemoryRegion *drom = g_new(MemoryRegion, 1);
     MemoryRegion *irom = g_new(MemoryRegion, 1);
+    MemoryRegion *icache0 = g_new(MemoryRegion, 1);
+    MemoryRegion *icache1 = g_new(MemoryRegion, 1);
 
     memory_region_init_ram(irom, NULL, "esp32.irom",
                            memmap[ESP32_MEMREGION_IROM].size, &error_fatal);
@@ -152,7 +112,17 @@ static void esp32_soc_realize(DeviceState *dev, Error **errp)
                            memmap[ESP32_MEMREGION_IRAM].size, &error_fatal);
     memory_region_add_subregion(sys_mem, memmap[ESP32_MEMREGION_IRAM].base, iram);
 
-    object_property_set_bool(OBJECT(&s->cpu), true, "realized", &error_abort);
+    memory_region_init_ram(icache0, NULL, "esp32.icache0",
+                               memmap[ESP32_MEMREGION_ICACHE0].size, &error_fatal);
+    memory_region_add_subregion(sys_mem, memmap[ESP32_MEMREGION_ICACHE0].base, icache0);
+
+    memory_region_init_ram(icache1, NULL, "esp32.icache1",
+                                   memmap[ESP32_MEMREGION_ICACHE1].size, &error_fatal);
+    memory_region_add_subregion(sys_mem, memmap[ESP32_MEMREGION_ICACHE1].base, icache1);
+
+    for (int i = 0; i < ms->smp.cpus; ++i) {
+        object_property_set_bool(OBJECT(&s->cpu[i]), true, "realized", &error_abort);
+    }
 
     object_property_set_bool(OBJECT(&s->uart), true, "realized", &error_abort);
 
@@ -164,24 +134,37 @@ static void esp32_soc_realize(DeviceState *dev, Error **errp)
     MemoryRegion *gpio = sysbus_mmio_get_region(SYS_BUS_DEVICE(&s->gpio), 0);
     memory_region_add_subregion_overlap(sys_mem, DR_REG_GPIO_BASE, gpio, 0);
 
-    create_unimplemented_device("esp32.dport", DR_REG_DPORT_BASE, 0x1000);
-    create_unimplemented_device("esp32.rtc_cntl", DR_REG_RTCCNTL_BASE, 0x1000);
+    MemoryRegion *dport = sysbus_mmio_get_region(SYS_BUS_DEVICE(&s->dport), 0);
+    memory_region_add_subregion(sys_mem, DR_REG_DPORT_BASE, dport);
+
+    MemoryRegion *rtc_cntl = sysbus_mmio_get_region(SYS_BUS_DEVICE(&s->rtc_cntl), 0);
+    memory_region_add_subregion(sys_mem, DR_REG_RTCCNTL_BASE, rtc_cntl);
+
+    create_unimplemented_device("esp32.frc0", DR_REG_FRC_TIMER_BASE, 0x1000);
+    create_unimplemented_device("esp32.rtcio", DR_REG_RTCIO_BASE, 0x400);
     create_unimplemented_device("esp32.uart1", DR_REG_UART1_BASE, 0x1000);
     create_unimplemented_device("esp32.tg0", DR_REG_TIMERGROUP0_BASE, 0x1000);
+    create_unimplemented_device("esp32.tg1", DR_REG_TIMERGROUP1_BASE, 0x1000);
     create_unimplemented_device("esp32.efuse", DR_REG_EFUSE_BASE, 0x1000);
     create_unimplemented_device("esp32.iomux", DR_REG_IO_MUX_BASE, 0x2000);
     create_unimplemented_device("esp32.hinf", DR_REG_HINF_BASE, 0x1000);
     create_unimplemented_device("esp32.slc", DR_REG_SLC_BASE, 0x1000);
     create_unimplemented_device("esp32.slchost", DR_REG_SLCHOST_BASE, 0x1000);
+    create_unimplemented_device("esp32.spi0", DR_REG_SPI0_BASE, 0x1000);
+    create_unimplemented_device("esp32.spi1", DR_REG_SPI1_BASE, 0x1000);
 
 }
 
 static void esp32_soc_init(Object *obj)
 {
     Esp32SocState *s = ESP32_SOC(obj);
-    SysBusDevice *sbd = SYS_BUS_DEVICE(obj);
+    MachineState *ms = MACHINE(qdev_get_machine());
 
-    object_initialize_child(obj, "cpu", &s->cpu, sizeof(s->cpu), TYPE_ESP32_CPU, &error_abort, NULL);
+    for (int i = 0; i < ms->smp.cpus; ++i) {
+        char name[16];
+        snprintf(name, sizeof(name), "cpu%d", i);
+        object_initialize_child(obj, name, &s->cpu[i], sizeof(s->cpu[i]), TYPE_ESP32_CPU, &error_abort, NULL);
+    }
 
     object_initialize_child(obj, "uart", &s->uart, sizeof(s->uart),
                             TYPE_ESP32_UART, &error_abort, NULL);
@@ -191,6 +174,12 @@ static void esp32_soc_init(Object *obj)
 
     object_initialize_child(obj, "gpio", &s->gpio, sizeof(s->gpio),
                                 TYPE_ESP32_GPIO, &error_abort, NULL);
+
+    object_initialize_child(obj, "dport", &s->dport, sizeof(s->dport),
+                            TYPE_ESP32_DPORT, &error_abort, NULL);
+
+    object_initialize_child(obj, "rtc_cntl", &s->rtc_cntl, sizeof(s->rtc_cntl),
+                            TYPE_ESP32_RTC_CNTL, &error_abort, NULL);
 }
 
 static Property esp32_soc_properties[] = {
@@ -232,8 +221,7 @@ static uint64_t translate_phys_addr(void *opaque, uint64_t addr)
 static void esp32_reset(void *opaque)
 {
     Esp32SocState *s = ESP32_SOC(opaque);
-
-    cpu_reset(CPU(&s->cpu));
+    esp32_soc_reset(DEVICE(s));
 }
 
 static void esp32_machine_inst_init(MachineState *machine)
@@ -256,11 +244,11 @@ static void esp32_machine_inst_init(MachineState *machine)
         uint64_t elf_entry;
         uint64_t elf_lowaddr;
         int success = load_elf(machine->kernel_filename, NULL,
-                               translate_phys_addr, &s->cpu,
+                               translate_phys_addr, &s->cpu[0],
                                &elf_entry, &elf_lowaddr,
                                NULL, 0, EM_XTENSA, 0, 0);
         if (success > 0) {
-            s->cpu.env.pc = elf_entry;
+            s->cpu[0].env.pc = elf_entry;
         }
     }
 }
@@ -270,6 +258,8 @@ static void esp32_machine_init(MachineClass *mc)
 {
     mc->desc = "Espressif ESP32 machine";
     mc->init = esp32_machine_inst_init;
+    mc->max_cpus = 2;
+    mc->default_cpus = 2;
 }
 
 DEFINE_MACHINE("esp32", esp32_machine_init)
