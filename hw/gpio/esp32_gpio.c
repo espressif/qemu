43f136f84a89400f20012ad60aa298a06637c1d3
Karol Nowak <knw@spyro-soft.com>
2023-02-28 13:21:01 +0100

    esp32_gpio: allow to set GPIO level from outside

--- hw/gpio/esp32_gpio.c
+++ hw/gpio/esp32_gpio.c
@@ -19,26 +19,205 @@
 #include "hw/qdev-properties.h"
 #include "hw/gpio/esp32_gpio.h"
 
+#include "qapi/qmp/qdict.h"
+#include "qapi/qmp/qjson.h"
+#include "qapi/qmp/qnum.h"
 
+// #define ENABLE_DEBUG
+
+#ifdef ENABLE_DEBUG
+#define D(x) x
+#else
+#define D(x) do {} while(0)
+#endif
+
+/*
+To start a tcp server of esp32.gpio:
+-chardev socket,port=17001,wait=no,host=0.0.0.0,server=on,id=esp32.gpio \
+-global driver=esp32.gpio,property=chardev,value=esp32.gpio \
+
+Event when output gpio(s) were modified:
+{"event":"new_out", "out":0, "out1":0}\r\n
+
+Message to set new value of input gpio(s):
+{"cmd":"set_input", "in":2147483648, "in1":1}\r\n
+{"cmd":"set_input", "in":16}\r\n
+
+4.12.1 GPIO Matrix Register Summary
+
+bitmask: each bit corresponds to one GPIO
+
+"in": GPIO31-GPIO0
+"in":2147483648 -> 0x80000000 -> MSB corresponds to GPIO31
+"in":1 -> 0x00000001 -> LSB corresponds to GPIO0
+
+"in1": GPIO39-GPIO32
+"in1": 128 -> 0x0x00000080 -> GPIO39
+"in1": 1 -> 0x0x00000001 -> GPIO32
+
+TODO:
+- improve handling of gpio_in_reg and gpio_in1_reg registers
+- should a firmware be able to change a state of GPIO when not enabled?
+*/
+
+#define HEX32_FMT "%08x"
+#define HEX64_FMT "%016lx"
+
+#define CRLF "\r\n"
+
+static void set_uint32_reg(QNum *num, uint32_t *reg, uint32_t mask)
+{
+    if(num != NULL && reg != NULL)
+    {
+        uint64_t value;
+        if(qnum_get_try_uint(num, &value))
+        {
+            D(qemu_log("server_read: reg=0x"HEX64_FMT"\n", value));
+
+            *reg = (uint32_t) value & mask;
+        }
+    }
+}
+
+static void server_read(void *opaque, const uint8_t *buf, int size)
+{
+    Esp32GpioState *s = ESP32_GPIO(opaque);
+
+    QEMU_LOCK_GUARD(&s->mutex);
+
+    /* find CRLF in buf */
+    char* crlf_pos = g_strstr_len((const char* ) buf, size, CRLF);
+    if(crlf_pos != NULL)
+    {
+        /* calc length */
+        uint8_t len = (const char*) crlf_pos - (const char*) buf; 
+
+        /* prepare string */
+        g_autoptr(GString) msg = g_string_new_len((const char*) buf, len);
+
+        /* decode json */
+        Error *err = NULL;
+        QDict *dict = qobject_to(QDict, qobject_from_json(msg->str, &err));
+        if (dict)
+        {
+            const char * cmd = qdict_get_try_str(dict, "cmd");
+            if(cmd != NULL)
+            {
+                if(strcmp(cmd, "set_input") == 0)
+                {
+                    set_uint32_reg(qobject_to(QNum, qdict_get(dict, "in")), &s->gpio_in_reg, 0xFFFFFFFF);
+                    set_uint32_reg(qobject_to(QNum, qdict_get(dict, "in1")), &s->gpio_in1_reg, 0x0000000F);
+                }
+            }
+
+            qobject_unref(dict);
+        }
+    }
+
+    /* ignore rest data of the buffer. Is it okay? */
+}
+
+
+static int server_can_read(void *opaque)
+{
+    return 256;
+}
+
+static void server_event(void *opaque, QEMUChrEvent event)
+{
+    switch (event)
+    {
+    case CHR_EVENT_OPENED:
+        D(qemu_log("esp32_gpio_server: client connected\n"));
+        break;
+    case CHR_EVENT_CLOSED:
+        D(qemu_log("esp32_gpio_server: client disconnected\n"));
+        break;
+    case CHR_EVENT_BREAK:
+    case CHR_EVENT_MUX_IN:
+    case CHR_EVENT_MUX_OUT:
+        break;
+    }
+}
 
 static uint64_t esp32_gpio_read(void *opaque, hwaddr addr, unsigned int size)
 {
+    D(qemu_log("esp32_gpio_read: addr=0x"HEX64_FMT"\n", addr));
+
     Esp32GpioState *s = ESP32_GPIO(opaque);
+
+    QEMU_LOCK_GUARD(&s->mutex);
+
     uint64_t r = 0;
     switch (addr) {
     case A_GPIO_STRAP:
         r = s->strap_mode;
         break;
-
+    case A_GPIO_IN_REG:
+        r = s->gpio_in_reg;
+        break;
+    case A_GPIO_IN1_REG:
+        r = s->gpio_in1_reg;
+        break;
     default:
         break;
     }
+
     return r;
 }
 
 static void esp32_gpio_write(void *opaque, hwaddr addr,
                        uint64_t value, unsigned int size)
 {
+    D(qemu_log("esp32_gpio_write: addr=0x"HEX64_FMT", value=0x"HEX64_FMT"\n", addr, value));
+
+    Esp32GpioState *s = ESP32_GPIO(opaque);
+
+    QEMU_LOCK_GUARD(&s->mutex);
+
+    uint32_t value32 = (uint32_t) value;
+
+    switch (addr) {
+    case A_GPIO_OUT_W1TS_REG:
+        s->gpio_out_reg |= value32;
+        break;
+    case A_GPIO_OUT_W1TC_REG:
+        s->gpio_out_reg &= ~value32;
+        break;
+    case A_GPIO_OUT1_W1TS_REG:
+        s->gpio_out1_reg |= value32;
+        break;
+    case A_GPIO_OUT1_W1TC_REG:
+        s->gpio_out1_reg &= ~value32;
+        break;
+    case A_GPIO_ENABLE_W1TS_REG:
+        s->gpio_enable_reg |= value32;
+        break;
+    case A_GPIO_ENABLE_W1TC_REG:
+        s->gpio_enable_reg &= ~value32;
+        break;
+    case A_GPIO_ENABLE1_W1TS_REG:
+        s->gpio_enable1_reg |= value32;
+        break;
+    case A_GPIO_ENABLE1_W1TC_REG:
+        s->gpio_enable1_reg &= ~value32;
+        break;
+    default:
+        break;
+    }
+
+    if(s->chardev)
+    {
+        g_autoptr(QDict) dict = qdict_new();
+        qdict_put_str(dict, "event", "new_out");
+        qdict_put_int(dict, "out", s->gpio_out_reg);
+        qdict_put_int(dict, "out1", s->gpio_out1_reg);
+
+        g_autoptr(GString) json = qobject_to_json(QOBJECT(dict));
+        g_string_append(json, CRLF);
+
+        qemu_chr_fe_write_all(&s->charbe, (uint8_t *)json->str, json->len);
+    }
 }
 
 static const MemoryRegionOps uart_ops = {
@@ -53,6 +232,22 @@ static void esp32_gpio_reset(DeviceState *dev)
 
 static void esp32_gpio_realize(DeviceState *dev, Error **errp)
 {
+    Esp32GpioState *s = ESP32_GPIO(dev);
+
+    if(s->chardev_name)
+    {
+        D(qemu_log("esp32_gpio_server: chardev_name=%s\n", s->chardev_name));
+
+        s->chardev = qemu_chr_find(s->chardev_name);
+        if (!s->chardev)
+        {
+            error_report("chardev '%s' not found", s->chardev_name);
+            return;
+        }
+
+        qemu_chr_fe_init(&s->charbe, s->chardev, &error_abort);
+        qemu_chr_fe_set_handlers(&s->charbe, server_can_read, server_read, server_event, NULL, dev, NULL, true);
+    }
 }
 
 static void esp32_gpio_init(Object *obj)
@@ -64,10 +259,13 @@ static void esp32_gpio_init(Object *obj)
                           TYPE_ESP32_GPIO, 0x1000);
     sysbus_init_mmio(sbd, &s->iomem);
     sysbus_init_irq(sbd, &s->irq);
+
+    qemu_mutex_init(&s->mutex);
 }
 
 static Property esp32_gpio_properties[] = {
     DEFINE_PROP_UINT32("strap_mode", Esp32GpioState, strap_mode, ESP32_STRAP_MODE_FLASH_BOOT),
+    DEFINE_PROP_STRING("chardev", Esp32GpioState, chardev_name),
     DEFINE_PROP_END_OF_LIST(),
 };
 
--- include/hw/gpio/esp32_gpio.h
+++ include/hw/gpio/esp32_gpio.h
@@ -4,10 +4,33 @@
 #include "hw/hw.h"
 #include "hw/registerfields.h"
 
+#include "chardev/char-fe.h"
+#include "chardev/char-io.h"
+#include "chardev/char-socket.h"
+
 #define TYPE_ESP32_GPIO "esp32.gpio"
 #define ESP32_GPIO(obj) OBJECT_CHECK(Esp32GpioState, (obj), TYPE_ESP32_GPIO)
 
+REG32(GPIO_OUT_REG, 0x0004)
+REG32(GPIO_OUT_W1TS_REG, 0x0008)
+REG32(GPIO_OUT_W1TC_REG, 0x000c)
+
+REG32(GPIO_OUT1_REG, 0x0010)
+REG32(GPIO_OUT1_W1TS_REG, 0x0014)
+REG32(GPIO_OUT1_W1TC_REG, 0x0018)
+
+REG32(GPIO_ENABLE_REG, 0x0020)
+REG32(GPIO_ENABLE_W1TS_REG, 0x0024)
+REG32(GPIO_ENABLE_W1TC_REG, 0x0028)
+
+REG32(GPIO_ENABLE1_REG, 0x002C)
+REG32(GPIO_ENABLE1_W1TS_REG, 0x0030)
+REG32(GPIO_ENABLE1_W1TC_REG, 0x0034)
+
 REG32(GPIO_STRAP, 0x0038)
+
+REG32(GPIO_IN_REG, 0x003c)
+REG32(GPIO_IN1_REG, 0x0040)
 
 #define ESP32_STRAP_MODE_FLASH_BOOT 0x12
 #define ESP32_STRAP_MODE_UART_BOOT  0x0f
@@ -18,5 +41,17 @@ typedef struct Esp32GpioState {
     MemoryRegion iomem;
     qemu_irq irq;
     uint32_t strap_mode;
+    uint32_t gpio_in_reg;
+    uint32_t gpio_in1_reg;
+
+    uint32_t gpio_out_reg;
+    uint32_t gpio_out1_reg;
+    uint32_t gpio_enable_reg;
+    uint32_t gpio_enable1_reg;
+
+    char* chardev_name;
+    Chardev *chardev;
+    CharBackend charbe;
+    QemuMutex mutex;
 } Esp32GpioState;
 
