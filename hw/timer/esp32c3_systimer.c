61a6bd5228abc208428446a18e177a0b08b612a7
xuchang-vivo <xuchang0929@vivo.com>
2026-04-01 19:07:56 +0800

    del fmt

--- hw/timer/esp32c3_systimer.c
+++ hw/timer/esp32c3_systimer.c
@@ -13,41 +13,42 @@
 #include "hw/boards.h"
 #include "hw/timer/esp32c3_systimer.h"
 
+
 static uint64_t esp32c3_systimer_read(void *opaque, hwaddr addr, unsigned int size)
 {
     ESP32C3SysTimerClass *class = ESP32C3_SYSTIMER_GET_CLASS(opaque);
     uint64_t r = 0;
 
-    switch (addr)
-    {
-    case A_SYSTIMER_CONF:
-        /* On the C3, bit 27 is always 0, bit 25 is always high */
-        r = class->parent_systimer_read(opaque, addr, size);
-        r &= ~BIT(27);
-        r |= BIT(25);
-        break;
+    switch (addr) {
+        case A_SYSTIMER_CONF:
+            /* On the C3, bit 27 is always 0, bit 25 is always high */
+            r = class->parent_systimer_read(opaque, addr, size);
+            r &= ~BIT(27);
+            r |= BIT(25);
+            break;
 
-    case A_SYSTIMER_REAL_TARGET0_LO:
-    case A_SYSTIMER_REAL_TARGET0_HI:
-    case A_SYSTIMER_REAL_TARGET1_LO:
-    case A_SYSTIMER_REAL_TARGET1_HI:
-    case A_SYSTIMER_REAL_TARGET2_LO:
-    case A_SYSTIMER_REAL_TARGET2_HI:
+        case A_SYSTIMER_REAL_TARGET0_LO:
+        case A_SYSTIMER_REAL_TARGET0_HI:
+        case A_SYSTIMER_REAL_TARGET1_LO:
+        case A_SYSTIMER_REAL_TARGET1_HI:
+        case A_SYSTIMER_REAL_TARGET2_LO:
+        case A_SYSTIMER_REAL_TARGET2_HI:
         /* These registers are not supported on the C3 hardware, do nothing! */
         break;
 
-    default:
-        r = class->parent_systimer_read(opaque, addr, size);
-        break;
+        default:
+            r = class->parent_systimer_read(opaque, addr, size);
+            break;
     }
 
     return r;
 }
 
+
 static void esp32c3_systimer_class_init(ObjectClass *klass, void *data)
 {
-    ESP32C3SysTimerClass *esp32c3 = ESP32C3_SYSTIMER_CLASS(klass);
-    ESPSysTimerClass *esp = ESP_SYSTIMER_CLASS(klass);
+    ESP32C3SysTimerClass* esp32c3 = ESP32C3_SYSTIMER_CLASS(klass);
+    ESPSysTimerClass* esp = ESP_SYSTIMER_CLASS(klass);
 
     /* Override the register read method */
     esp32c3->parent_systimer_read = esp->systimer_ops.read;
